28beecf0586dfda7098f0b51d6d387e261c56b83
Olivier Cuypers <oliviercuyp@gmail.com>
2021-01-05 18:29:25 +0100

    fix(index): correct the awsome feature release

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// Feat awsome, commit 1
+// Feat awsome, commit 2
 const config = require('config');
 const server = require('./server');
 const log = require('saga-logger').create({ module: module.id });
